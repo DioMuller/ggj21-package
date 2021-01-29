20a5ff5aed15514bf0d8005f294f27af2ca6c653
Diogo Muller <diogo.muller@live.com>
2021-01-28 23:28:28 -0300

    DEV: Created project and character movement.

---
+++ Source/Lost In Customs/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    #region Editor Attributes
+    [SerializeField, Tooltip("Player Look Sensivity")] 
+    private float sensitivity = 150;
+
+    [SerializeField, Tooltip("Player Up/Down angle limit")] 
+    private float lookLimit = 45;
+    
+    [SerializeField, Tooltip("Camera Game Object")] 
+    private Transform cameraTransform = null;
+    #endregion // Editor Attributes
+
+    #region Private Attributes
+    private float _rotationX;
+    #endregion // Private Attributes
+
+    #region MonoBehaviour Methods
+    // Update is called once per frame
+    void Update()
+    {
+        float lx = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float ly = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        _rotationX = Mathf.Clamp(_rotationX - ly, -lookLimit, lookLimit);
+
+        cameraTransform.localRotation = Quaternion.Euler(_rotationX, 0f,0f);
+        transform.Rotate(Vector2.up * lx);
+    }
+    #endregion // MonoBehaviour Methods
+}
